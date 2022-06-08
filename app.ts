215581b62d86765cd05685a359a3484e5beb6e03
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-06-08 11:15:36 -0300

    desafio 2

--- app.ts
+++ app.ts
@@ -4,3 +4,39 @@ let employee : { code: number, name: string } = {
   name: "John"
 };
 
+//desafio 2: 
+
+enum profissao {
+  Atriz,
+  Padeiro
+}
+
+interface IUsers {
+  nome: string;
+  idade: number;
+  profissao: profissao;
+}
+
+let pessoa1: IUsers = {
+  nome : "maria",
+  idade : 29,
+  profissao : profissao.Atriz,
+}
+
+let pessoa2: IUsers  = {
+  nome : "roberto",
+  idade : 19,
+  profissao : profissao.Padeiro,
+}
+
+let pessoa3: IUsers  = {
+    nome: "laura",
+    idade: 32,
+    profissao: profissao.Atriz
+};
+
+let pessoa4 : IUsers  = {
+    nome: "carlos",
+    idade: 19,
+    profissao: profissao.Padeiro
+}
